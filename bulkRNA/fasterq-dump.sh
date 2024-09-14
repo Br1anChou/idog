809a786d8ca17f64e32282909d8466b2541f3704
BrianChou <zbw550502851@gmail.com>
2024-09-14 17:49:54 +0800

    Update fasterq-dump.sh

--- bulkRNA/fasterq-dump.sh
+++ bulkRNA/fasterq-dump.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
+# @Usage      :bash fasterq-dump.sh <sra_dir> <out_dir> <sample_id> <ncpus>
+
 ###software vision###
 #sratoolkit:3.0.2
 #pigz:2.4
 
 ###parameter###
-#sra_dir=/p300s/zhaowm_group/sunjiani/projects/RNA_seq/PRJNA847879/SRR19614475
 sra_dir=$1
-#out_dir=/p300s/zhaowm_group/tangbx/idog/test
 out_dir=$2
-#sample_id=SRR19614475
 sample_id=$3
-#ncpus=10
 ncpus=$4
 ##############
 
